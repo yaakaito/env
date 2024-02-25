95b01f725be6a1f9d80bd6c821a94cd593a024ea
yaakaito <yaakaito@gmail.com>
2024-02-26 06:39:26 +0900

    Create vscode-extensions.zsh

---
+++ vscode-extensions.zsh
@@ -0,0 +1,8 @@
+if [ ! -f ~/.zsh/.vscode-extension-installed ]; then
+  code --install-extension github.copilot
+  code --install-extension github.copilot-chat
+  code --install-extension mhutchie.git-graph
+  code --install-extension eamodio.gitlens
+  code --install-extension quick-lint.quick-lint-js
+  touch ~/.zsh/.vscode-extension-installed
+fi
