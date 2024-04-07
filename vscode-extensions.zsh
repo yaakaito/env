b88229efdc51b18f184b5109570e8b9b7787f8b4
UKYO Kazuma <yaakaito@gmail.com>
2024-04-08 02:11:45 +0900

    Update vscode-extensions.zsh

--- vscode-extensions.zsh
+++ vscode-extensions.zsh
@@ -1,8 +1,29 @@
-if [ ! -f ~/.zsh/.vscode-extensions ]; then
-  code --install-extension github.copilot
-  code --install-extension github.copilot-chat
-  code --install-extension mhutchie.git-graph
-  code --install-extension eamodio.gitlens
-  code --install-extension quick-lint.quick-lint-js
-  touch ~/.zsh/.vscode-extensions
+code_exists() {
+  code -v "$1" &> /dev/null
+}
+
+if code_exists code; then
+  echo "VSCode拡張機能のインストールを開始します..."
+  if [ ! -f ~/.zsh/.vscode-extensions ]; then
+    code --install-extension github.copilot
+    code --install-extension github.copilot-chat
+    code --install-extension mhutchie.git-graph
+    code --install-extension eamodio.gitlens
+    code --install-extension quick-lint.quick-lint-js
+    touch ~/.zsh/.vscode-extensions
+  fi
+fi
+
+cursor_exists() {
+  cursor -v "$1" &> /dev/null
+}
+
+if cursor_exists cursor; then
+  echo "Cursor拡張機能のインストールを開始します..."
+  if [ ! -f ~/.zsh/.cursor-extensions ]; then
+    cursor --install-extension mhutchie.git-graph
+    cursor --install-extension eamodio.gitlens
+    cursor --install-extension quick-lint.quick-lint-js
+    touch ~/.zsh/.cursor-extensions
+  fi
 fi
