7e12d9bc5384ac8b722275f5e3be174f212dcc38
yaakaito <yaakaito@gmail.com>
2026-03-23 01:54:22 +0900

    chore: vscode-plugins を整理

--- dotfiles/zsh/vscode-extensions.zsh
+++ dotfiles/zsh/vscode-extensions.zsh
@@ -1,10 +1,10 @@
 if [ ! -f ~/.zsh/.vscode-extensions ]; then
   echo "VSCode拡張機能のインストールを開始します..."
-  code --install-extension github.copilot
+  # code --install-extension github.copilot
   code --install-extension github.copilot-chat
-  code --install-extension mhutchie.git-graph
-  code --install-extension eamodio.gitlens
-  code --install-extension quick-lint.quick-lint-js
-  code --install-extension RooVeterinaryInc.roo-cline
+  # code --install-extension mhutchie.git-graph
+  # code --install-extension eamodio.gitlens
+  # code --install-extension quick-lint.quick-lint-js
+  # code --install-extension RooVeterinaryInc.roo-cline
   touch ~/.zsh/.vscode-extensions
 fi
